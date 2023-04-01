a45ab168b7ca154856b35afdc5688096223994bf
Jorge Gonzalez <jorgelg@usc.edu>
2023-04-01 07:30:46 -0700

    debounced button.py

--- sensors/button.py
+++ sensors/button.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD) 
@@ -7,3 +8,5 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True: 
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
+        while(GPIO.input(10)==GPIO.HIGH):
+            time.sleep(15/1000)
