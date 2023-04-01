76f676156888f158f1ad169d2bb6d0f6ff082bc0
Julia Wang <jlwang@usc.edu>
2023-04-01 15:58:27 -0700

    update server

--- tcp_server_client/socket_echo_server.py
+++ tcp_server_client/socket_echo_server.py
@@ -4,35 +4,39 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('172.20.10.7', 12345)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+    # Bind the socket to the port
+    server_address = ('172.20.10.7', 12345)
+    print('starting up on {} port {}'.format(*server_address))
+    sock.bind(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
+    # Listen for incoming connections
+    sock.listen(1)
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
+    while True:
+        # Wait for a connection
+        print('waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            print('connection from', client_address)
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print('received {!r}'.format(data))
-            if data:
-                print('sending data back to the client')
-                connection.sendall(data)
-            else:
-                print('no data from', client_address)
-                break
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                print('received {!r}'.format(data))
+                if data:
+                    print('sending data back to the client')
+                    connection.sendall(data)
+                else:
+                    print('no data from', client_address)
+                    break
 
-    finally:
-        # Clean up the connection
-        connection.close()
+        finally:
+            # Clean up the connection
+            connection.close()
+
+if __name__ == "__main__":
+    main()
