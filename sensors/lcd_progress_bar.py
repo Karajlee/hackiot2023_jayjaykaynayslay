b79b1d193f5fa694997bf466f1914cadbe83a9c9
Julia Wang <jlwang@usc.edu>
2023-04-02 03:10:45 -0700

    reformat laksdjg

--- sensors/lcd_progress_bar.py
+++ sensors/lcd_progress_bar.py
@@ -127,7 +127,6 @@ while (col >= 0):
     col -= 2
     time.sleep(0.15)
 
-
 while True:
     if GPIO.input(26) == GPIO.HIGH:
         print("Button was pushed!")
@@ -150,121 +149,116 @@ while True:
     load_bar = int(value * 7 / 96)
     print(value, ' ', load_bar)
 
-    # LOAD BAR
-    if (load_bar != 7):
-        print("LOADING")
-    # PEACE LOVE CODE
-    else:
-        # PEACE
-        peaceL = [
-            0b00000,
-            0b00000,
-            0b00001,
-            0b00001,
-            0b00001,
-            0b00001,
-            0b00000,
-            0b00000,
-        ]
-        peaceM = [
-            0b01110,
-            0b10101,
-            0b00100,
-            0b00100,
-            0b00100,
-            0b01010,
-            0b10001,
-            0b01110,
-        ]
-        peaceR = [
-            0b00000,
-            0b00000,
-            0b10000,
-            0b10000,
-            0b10000,
-            0b10000,
-            0b00000,
-            0b00000,
-        ]
-        # Load custom characters into LCD memory
-        lcd.create_char(0, peaceL)
-        lcd.create_char(1, peaceM)
-        lcd.create_char(2, peaceR)
-        # Display custom characters using message()
-        lcd.message('\n')
-        lcd.set_cursor(1, 0)
-        lcd.message('\x00')  # Display heart character
-        lcd.message('\x01')  # Display smiley character
-        lcd.message('\x02')  # Display smiley character
-
-        # HEART
-        heartL = (
-            0b00110,
-            0b01111,
-            0b11111,
-            0b11111,
-            0b01111,
-            0b00111,
-            0b00011,
-            0b00001,
-        )
-        heartR = (
-            0b01100,
-            0b11110,
-            0b11111,
-            0b11111,
-            0b11110,
-            0b11100,
-            0b11000,
-            0b10000,
-        )
-        # Load custom characters into LCD memory
-        lcd.create_char(3, heartL)
-        lcd.create_char(4, heartR)
-        # Display custom characters using message()
-        lcd.message('\n')
-        lcd.set_cursor(7, 0)
-        lcd.message('\x03')  # Display heart character
-        lcd.message('\x04')  # Display smiley character
-
-
-        # CODE
-        codeL = (
-            0b00010,
-            0b00100,
-            0b01000,
-            0b10000,
-            0b01000,
-            0b00100,
-            0b00010,
-            0b00000,
-        )
-        codeR = (
-            0b01000,
-            0b00100,
-            0b00010,
-            0b00001,
-            0b00010,
-            0b00100,
-            0b01000,
-            0b00000,
-        )
-        # Load custom characters into LCD memory
-        lcd.create_char(5, codeL)
-        lcd.create_char(6, codeR)
-        # Display custom characters using message()
-        lcd.message('\n')
-        lcd.set_cursor(12, 0)
-        lcd.message('\x05')  # Display heart character
-        lcd.message('\x06')  # Display smiley character
-
-
-        # PEACE LOVE CODE
-        lcd.set_cursor(0, 1)
-        lcd.message('Peace Love Code')
-
-        time.sleep(2)
-
+    if (load_bar == 7):
         break
 
     time.sleep(.5)
+
+# PEACE
+peaceL = [
+    0b00000,
+    0b00000,
+    0b00001,
+    0b00001,
+    0b00001,
+    0b00001,
+    0b00000,
+    0b00000,
+]
+peaceM = [
+    0b01110,
+    0b10101,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b01010,
+    0b10001,
+    0b01110,
+]
+peaceR = [
+    0b00000,
+    0b00000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b00000,
+    0b00000,
+]
+# Load custom characters into LCD memory
+lcd.create_char(0, peaceL)
+lcd.create_char(1, peaceM)
+lcd.create_char(2, peaceR)
+# Display custom characters using message()
+lcd.message('\n')
+lcd.set_cursor(1, 0)
+lcd.message('\x00')  # Display heart character
+lcd.message('\x01')  # Display smiley character
+lcd.message('\x02')  # Display smiley character
+
+
+# HEART
+heartL = (
+    0b00110,
+    0b01111,
+    0b11111,
+    0b11111,
+    0b01111,
+    0b00111,
+    0b00011,
+    0b00001,
+)
+heartR = (
+    0b01100,
+    0b11110,
+    0b11111,
+    0b11111,
+    0b11110,
+    0b11100,
+    0b11000,
+    0b10000,
+)
+# Load custom characters into LCD memory
+lcd.create_char(3, heartL)
+lcd.create_char(4, heartR)
+# Display custom characters using message()
+lcd.message('\n')
+lcd.set_cursor(7, 0)
+lcd.message('\x03')  # Display heart character
+lcd.message('\x04')  # Display smiley character
+
+
+# CODE
+codeL = (
+    0b00010,
+    0b00100,
+    0b01000,
+    0b10000,
+    0b01000,
+    0b00100,
+    0b00010,
+    0b00000,
+)
+codeR = (
+    0b01000,
+    0b00100,
+    0b00010,
+    0b00001,
+    0b00010,
+    0b00100,
+    0b01000,
+    0b00000,
+)
+# Load custom characters into LCD memory
+lcd.create_char(5, codeL)
+lcd.create_char(6, codeR)
+# Display custom characters using message()
+lcd.message('\n')
+lcd.set_cursor(12, 0)
+lcd.message('\x05')  # Display heart character
+lcd.message('\x06')  # Display smiley character
+
+
+# PEACE LOVE CODE
+lcd.set_cursor(0, 1)
+lcd.message('Peace Love Code')
