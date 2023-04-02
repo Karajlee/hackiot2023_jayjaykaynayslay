2594d03935f87c71d5b8f363cada0d168331b84a
Kara Lee <karajlee@usc.edu>
2023-04-01 17:09:00 -0700

    lcd

--- sensors/state_machine.py
+++ sensors/state_machine.py
@@ -5,8 +5,25 @@ import time
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(11, GPIO.OUT)  # Backlight
+GPIO.output(11, GPIO.LOW)
 GPIO.setup(12, GPIO.OUT)  # LED
 GPIO.output(12, GPIO.LOW) # OFF
+# GPIO Pins for LCD and Backlight
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 4
+
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
 
 # initialize state
 class State(Enum):
@@ -15,6 +32,11 @@ class State(Enum):
     STATE_1 = 3
     STATE_2 = 4
 state = State.OFF
+
+def state_2_lcd_messages():
+    lcd.clear()
+    lcd.message("Message sent to Client")
+    print("Message sent to Client")
 
 while True: 
     if GPIO.input(10) == GPIO.HIGH:
@@ -26,13 +48,14 @@ while True:
             GPIO.output(12, GPIO.HIGH)
         elif state == State.ON:
             state = State.STATE_1
-            GPIO.output(12, GPIO.HIGH)
         elif state == State.STATE_1:
             state = State.STATE_2
-            GPIO.output(12, GPIO.HIGH)
+            GPIO.output(11, GPIO.HIGH)
+            state_2_lcd_messages()
         elif state == State.STATE_2:
             state = State.OFF
             GPIO.output(12, GPIO.LOW)
+            GPIO.output(11, GPIO.LOW)
         
         print("State:", state)
 
