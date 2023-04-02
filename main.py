e24a96abc987557b145da53ec899a5761a7c980e
Julia Wang <jlwang@usc.edu>
2023-04-02 06:26:59 -0700

    fucking mutex

--- main.py
+++ main.py
@@ -181,6 +181,7 @@ def init_rpi1():
         client_sock.close()
 
 # ----------------------------- THREADS -----------------------------
+m = threading.Lock()
 def read_from_client(socket, address):
     global pwm
     global lcd
@@ -208,175 +209,64 @@ def read_from_client(socket, address):
         if(sensor_data[0]=="R"):
             total_cols = int(sensor_data[1])
             print(total_cols)
+            m.acquire()
             if total_cols != 7:
                 print("Total cols != 7")
-
-                lcd.clear()
-                print("Total cols = 7")
-                # PEACE
-                peaceL = [
+                # Filled square
+                fill = [
+                    0b11111,
+                    0b11111,
+                    0b11111,
+                    0b11111,
+                    0b11111,
+                    0b11111,
+                    0b11111,
+                    0b11111,
+                ]
+                # Empty square
+                empty = [
                     0b00000,
                     0b00000,
-                    0b00001,
-                    0b00001,
-                    0b00001,
-                    0b00001,
+                    0b00000,
+                    0b00000,
+                    0b00000,
+                    0b00000,
                     0b00000,
                     0b00000,
                 ]
-                peaceM = [
-                    0b01110,
-                    0b10101,
-                    0b00100,
-                    0b00100,
-                    0b00100,
-                    0b01010,
-                    0b10001,
-                    0b01110,
-                ]
-                peaceR = [
-                    0b00000,
-                    0b00000,
-                    0b10000,
-                    0b10000,
-                    0b10000,
-                    0b10000,
-                    0b00000,
-                    0b00000,
-                ]
-                # Load custom characters into LCD memory
-                lcd.create_char(0, peaceL)
-                lcd.create_char(1, peaceM)
-                lcd.create_char(2, peaceR)
-                # Display custom characters using message()
-                lcd.message('\n')
-                lcd.set_cursor(1, 0)
-                lcd.message('\x00')  # Display heart character
-                lcd.message('\x01')  # Display smiley character
-                lcd.message('\x02')  # Display smiley character
+                lcd.create_char(0, fill)
+                lcd.create_char(7, empty)
 
 
-                # HEART
-                heartL = (
-                    0b00110,
-                    0b01111,
-                    0b11111,
-                    0b11111,
-                    0b01111,
-                    0b00111,
-                    0b00011,
-                    0b00001,
-                )
-                heartR = (
-                    0b01100,
-                    0b11110,
-                    0b11111,
-                    0b11111,
-                    0b11110,
-                    0b11100,
-                    0b11000,
-                    0b10000,
-                )
-                # Load custom characters into LCD memory
-                lcd.create_char(3, heartL)
-                lcd.create_char(4, heartR)
-                # Display custom characters using message()
-                lcd.message('\n')
-                lcd.set_cursor(7, 0)
-                lcd.message('\x03')  # Display heart character
-                lcd.message('\x04')  # Display smiley character
+                max_col = 2 * total_cols
+                col = 0
+                while (col < max_col):
+                    # Load custom characters into LCD memory
+                    # Display custom characters using message()
+                    lcd.message('\n')
+                    lcd.set_cursor(col, 0)
+                    lcd.message('\x00')
+                    lcd.set_cursor(col+1, 0)
+                    lcd.message('\x00')
+                    lcd.set_cursor(col, 1)
+                    lcd.message('\x00')
+                    lcd.set_cursor(col+1, 1)
+                    lcd.message('\x00')
 
+                    col += 2
+                    # time.sleep(0.3)
 
-                # CODE
-                codeL = (
-                    0b00010,
-                    0b00100,
-                    0b01000,
-                    0b10000,
-                    0b01000,
-                    0b00100,
-                    0b00010,
-                    0b00000,
-                )
-                codeR = (
-                    0b01000,
-                    0b00100,
-                    0b00010,
-                    0b00001,
-                    0b00010,
-                    0b00100,
-                    0b01000,
-                    0b00000,
-                )
-                # Load custom characters into LCD memory
-                lcd.create_char(5, codeL)
-                lcd.create_char(6, codeR)
-                # Display custom characters using message()
-                lcd.message('\n')
-                lcd.set_cursor(12, 0)
-                lcd.message('\x05')  # Display heart character
-                lcd.message('\x06')  # Display smiley character
+                col = max_col
+                while (col < 16):
+                    # Load custom characters into LCD memory
+                    # Display custom characters using message()
+                    lcd.message('\n')
+                    lcd.set_cursor(col, 0)
+                    lcd.message('\x07')
+                    lcd.set_cursor(col, 1)
+                    lcd.message('\x07')
 
-
-                # PEACE LOVE CODE
-                lcd.set_cursor(0, 1)
-                lcd.message('Peace Love CodE')
-                print("lcd done print")
-                # # Filled square
-                # fill = [
-                #     0b11111,
-                #     0b11111,
-                #     0b11111,
-                #     0b11111,
-                #     0b11111,
-                #     0b11111,
-                #     0b11111,
-                #     0b11111,
-                # ]
-                # # Empty square
-                # empty = [
-                #     0b00000,
-                #     0b00000,
-                #     0b00000,
-                #     0b00000,
-                #     0b00000,
-                #     0b00000,
-                #     0b00000,
-                #     0b00000,
-                # ]
-                # lcd.create_char(0, fill)
-                # lcd.create_char(7, empty)
-
-
-                # max_col = 2 * total_cols
-                # col = 0
-            #     while (col < max_col):
-            #         # Load custom characters into LCD memory
-            #         # Display custom characters using message()
-            #         lcd.message('\n')
-            #         lcd.set_cursor(col, 0)
-            #         lcd.message('\x00')
-            #         lcd.set_cursor(col+1, 0)
-            #         lcd.message('\x00')
-            #         lcd.set_cursor(col, 1)
-            #         lcd.message('\x00')
-            #         lcd.set_cursor(col+1, 1)
-            #         lcd.message('\x00')
-
-            #         col += 2
-            #         # time.sleep(0.3)
-
-            #     col = max_col
-            #     while (col < 16):
-            #         # Load custom characters into LCD memory
-            #         # Display custom characters using message()
-            #         lcd.message('\n')
-            #         lcd.set_cursor(col, 0)
-            #         lcd.message('\x07')
-            #         lcd.set_cursor(col, 1)
-            #         lcd.message('\x07')
-
-            #         col += 1
+                    col += 1
                     # time.sleep(0.15)
 
             else:
@@ -491,6 +381,7 @@ def read_from_client(socket, address):
                 lcd.set_cursor(0, 1)
                 lcd.message('Peace Love Code')
                 print("lcd done print")
+            m.release()
 
     # global state
     # state = State.OFF
