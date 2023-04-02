2d06f3182d7dbbb09181629f42df63ad1fec544e
Jorge Gonzalez <jorgelg@usc.edu>
2023-04-01 22:11:55 -0700

    pleaseeeeeeeee

--- sensors/lcd.py
+++ sensors/lcd.py
@@ -47,10 +47,11 @@ pwm = GPIO.PWM(lcd_backlight, 100)
 lcd_columns = 16
 lcd_rows = 2
 
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
 lcd.clear()
 lcd.message("Please")
-# lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-#                            lcd_columns, lcd_rows, lcd_backlight)
 
 # curr = 0
 # pwm.start(curr)
