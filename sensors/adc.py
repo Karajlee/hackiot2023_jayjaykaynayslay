a2b7dc937cce036f983c0726e6a8144f68e56a4a
Jorge Gonzalez <jorgelg@usc.edu>
2023-04-01 17:55:05 -0700

    new adc test

--- sensors/adc.py
+++ sensors/adc.py
@@ -1,13 +1,17 @@
 import spidev
-from gpiozero import MCP3008
 
 spi = spidev.SpiDev()
-spi.open(0,0)
+spi.open(0, 0)  # open SPI bus 0, device 0
+spi.max_speed_hz = 1000000  # set SPI clock speed
 
-adc = MCP3008(channel=0)
+def read_adc(channel):
+    # MCP3008 expects 3 bytes: start bit, single-ended/differential bit, and channel selection bits
+    # We can send 3 bytes at once using spi.xfer2()
+    r = spi.xfer2([1, (8 + channel) << 4, 0])
+    # The ADC returns 10 bits of data, but the first 2 bits are meaningless. We can discard them by taking the last 8 bits.
+    adc = ((r[1] & 3) << 8) + r[2]
+    return adc
 
 while True:
-
-    analog_value=adc.value
-
-    print(analog_value)
+    value = read_adc(0)  # read from channel 0
+    print("ADC value:", value)
