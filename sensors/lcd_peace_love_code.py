487470da82755b5302dbec1c926b374e1406e0c0
Jorge Gonzalez <jorgelg@usc.edu>
2023-04-01 22:55:29 -0700

    lcd 8-bit

--- sensors/lcd_peace_love_code.py
+++ sensors/lcd_peace_love_code.py
@@ -89,62 +89,62 @@ peaceR = (
 )
 lcd.set_cursor(1, 0)
 lcd.create_char(0, peaceL)
-lcd.write_string(unichr(0))
+lcd.write8(unichr(0))
 lcd.create_char(1, peaceM)
-lcd.write_string(unichr(1))
+lcd.write8(unichr(1))
 lcd.create_char(2, peaceR)
-lcd.write_string(unichr(2))
+lcd.write8(unichr(2))
 
-# Heart
-heartL = (
-    0b00110,
-    0b01111,
-    0b11111,
-    0b11111,
-    0b01111,
-    0b00111,
-    0b00011,
-    0b00001,
-)
-heartR = (
-    0b01100,
-    0b11110,
-    0b11111,
-    0b11111,
-    0b11110,
-    0b11100,
-    0b11000,
-    0b10000,
-)
-lcd.set_cursor(7, 0)
-lcd.create_char(3, heartL)
-lcd.write_string(unichr(3))
-lcd.create_char(4, heartR)
-lcd.write_string(unichr(4))
+# # Heart
+# heartL = (
+#     0b00110,
+#     0b01111,
+#     0b11111,
+#     0b11111,
+#     0b01111,
+#     0b00111,
+#     0b00011,
+#     0b00001,
+# )
+# heartR = (
+#     0b01100,
+#     0b11110,
+#     0b11111,
+#     0b11111,
+#     0b11110,
+#     0b11100,
+#     0b11000,
+#     0b10000,
+# )
+# lcd.set_cursor(7, 0)
+# lcd.create_char(3, heartL)
+# lcd.write_string(unichr(3))
+# lcd.create_char(4, heartR)
+# lcd.write_string(unichr(4))
 
-# Code
-codeL = (
-    0b00010,
-    0b00100,
-    0b01000,
-    0b10000,
-    0b01000,
-    0b00100,
-    0b00010,
-    0b00000,
-)
-codeR = (
-    0b01000,
-    0b00100,
-    0b00010,
-    0b00001,
-    0b00010,
-    0b00100,
-    0b01000,
-    0b00000,
-)
-lcd.set_cursor(12, 0)
-lcd.create_char(5, codeL)
-lcd.write_string(unichr(5))
-lcd.create_char(6, codeR)
-lcd.write_string(unichr(6))
+# # Code
+# codeL = (
+#     0b00010,
+#     0b00100,
+#     0b01000,
+#     0b10000,
+#     0b01000,
+#     0b00100,
+#     0b00010,
+#     0b00000,
+# )
+# codeR = (
+#     0b01000,
+#     0b00100,
+#     0b00010,
+#     0b00001,
+#     0b00010,
+#     0b00100,
+#     0b01000,
+#     0b00000,
+# )
+# lcd.set_cursor(12, 0)
+# lcd.create_char(5, codeL)
+# lcd.write_string(unichr(5))
+# lcd.create_char(6, codeR)
+# lcd.write_string(unichr(6))
