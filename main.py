35cfe3f45561cd52d132ef7ac0e41a3c88edd5e2
Julia Wang <jlwang@usc.edu>
2023-04-02 05:12:56 -0700

    FUCK

--- main.py
+++ main.py
@@ -448,7 +448,7 @@ def write_to_client(socket):
 
         if state == State.SEND_MSG:
             msg_str = "R "+value_str
-            print(msg_str)
+            print(value, ' | ', msg_str)
             socket.sendall(msg_str.encode('utf-8'))
             # state = State.SEND_PRESSURE
         elif state == State.SEND_PRESSURE:
